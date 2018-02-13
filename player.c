4d16bc5ae770a32742546a7f3b3f642b5d95d0dd
Brandon Martin <bsm2112@yahoo.com>
2018-02-13 17:01:03 -0600

    test

--- player.c
+++ player.c
@@ -2,7 +2,7 @@
 
 struct Player* createPlayer(char* name, char symbol, int idNum) {
     struct Player* p = malloc(sizeof(struct Player));
-    strcpy(p->name, name);
+    strcpy(p->name, name); //a comment
     p -> symbol = symbol;
     p -> idNumber = idNum;
     return p;

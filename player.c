65ce794d72188d9fae2632a6b55fa65d044b7f13
Brandon Martin <bsm2112@yahoo.com>
2018-02-13 17:02:10 -0600

    swap

--- player.c
+++ player.c
@@ -1,8 +1,9 @@
 #include "header.h"
 
 struct Player* createPlayer(char* name, char symbol, int idNum) {
+    // this is a comment
     struct Player* p = malloc(sizeof(struct Player));
-    strcpy(p->name, name); //a comment
+    strcpy(p->name, name);
     p -> symbol = symbol;
     p -> idNumber = idNum;
     return p;
